ce409c1e206c90f944d547989bce07afeb28f01e
Devansh-Walia <devanshwalia9898@gmail.com>
2024-04-29 01:17:54 +0530

    Update config/index.ts with base URL and database port configuration

--- config/index.ts
+++ config/index.ts
@@ -2,6 +2,7 @@ import "dotenv/config";
 
 const applicationConfig = {
   port: process.env.PORT || 3000,
+  base: process.env.BASE_URL || "http://localhost:3000",
   // Database
   db: {
     dbDialect: process.env.DB_DIALECT || "postgres",
@@ -9,7 +10,7 @@ const applicationConfig = {
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     name: process.env.DB_NAME,
-    port: process.env.DB_PORT,
+    port: Number(process.env.DB_PORT) || 5432,
   },
 };
 
